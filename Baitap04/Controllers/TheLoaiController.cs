58d167ae664d3fae912e29bd95d1b9e7bb0b765f
Luong Duong Thanh Dat <luongduongthanhdat0911@gmail.com>
2024-09-04 14:19:31 +0700

    hoan thanh bai tap 4

---
+++ Baitap04/Controllers/TheLoaiController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Baitap04.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Edit( int id)
+        {
+            return Ok("id: " + id);
+        }
+        public IActionResult Detail(int id, string ten)
+        {
+            //return Content("id: " + id + "; ten: "+ ten);
+            return Content(string.Format("id: {0}; ten: {1}", id, ten));
+        }
+        public IActionResult Show(List<string> categories)
+        {
+            string content = " Danh sach the loai: ";
+            foreach (var category in categories)
+            {
+                content = content +" " + category + ", ";                      
+            }
+            return Content(content);
+        }
+
+    }
+}
