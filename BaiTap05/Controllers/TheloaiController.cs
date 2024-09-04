3917d2ea5da75b62d6d148757c836d59ecdde4d4
Luong Duong Thanh Dat <luongduongthanhdat0911@gmail.com>
2024-09-04 14:58:49 +0700

    hoanh thanh

---
+++ BaiTap05/Controllers/TheloaiController.cs
@@ -0,0 +1,25 @@
+﻿using BaiTap05.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTap05.Controllers
+{
+    public class TheloaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Ngay = " Ngay 28";
+            ViewBag.Thang = " Thang 2";
+            ViewData["Nam"] = " Nam 2030";
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            var theloai = new TheLoaiViewModel
+            {
+                Id = 1,
+                Name = " Trinh Tham"
+            };
+            return View(theloai);
+        }
+    }
+}
